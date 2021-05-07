7c0c341736b40c4712fbcc84c24e06eb6b285602
MatheusD3V <matheus.gomes@pixter.com.br>
2021-05-06 23:08:48 -0300

    fix(server): cors problem solved

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,19 @@ import "./database";
 
 import express from "express";
 import env from "./config/environments";
+import cors from "cors";
 
 import { router } from "./routes";
+
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 const app = express();
 
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use(router);
 
 app.listen(env.port, () => {
